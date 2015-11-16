98bf9521290f5807b945eea30d74cb83bfd150ca
Medovik <scott_hall_88@mail.ru>
2015-11-17 00:49:08 +0200

    Test
    test

---
+++ app/src/main/java/moviemania/any/com/moviemania/Movie.java
@@ -0,0 +1,11 @@
+package moviemania.any.com.moviemania;
+
+import android.app.Activity;
+
+/**
+ * Created by Юра on 17.11.2015.
+ */
+public class Movie extends Activity
+{
+    public String name;
+}
